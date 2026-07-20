ef8dd064588b7961975b724ecbec4b9892404add
Alexander <stdAlexTikhonov@hotmail.com>
2026-07-20 18:32:00 +0300

    fix(TASK-2): added nojekyl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import path from 'path'
+import path, { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +17,16 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    {
+      name: 'copy-nojekyll',
+      closeBundle() {
+        // Копируем .nojekyll в папку docs после сборки
+        copyFileSync(
+          resolve(__dirname, '.nojekyll'),
+          resolve(__dirname, 'docs', '.nojekyll')
+        );
+      }
+    }
   ],
 })
