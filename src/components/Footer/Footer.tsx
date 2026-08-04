4588dbba5c582634e60637ef94ed0157fcd31ecd
Alexander <stdAlexTikhonov@hotmail.com>
2026-08-04 16:22:06 +0300

    fix(TASK-10): added footer translations

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,25 @@
-// Footer.tsx
 import styles from './Footer.module.scss';
 import { PolicyIcon, ContactsIcon, GithubIcon } from './icons';
+import { useTranslation } from 'react-i18next';
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const { t } = useTranslation();
 
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <p className={styles.copyright}>
-          &copy; {currentYear} Все права защищены
+          &copy; {t('footer.rights', { currentYear })}
         </p>
         <nav className={styles.links}>
           <a href="#" className={styles.link}>
             <PolicyIcon />
-            <span className={styles.text}>Политика конфиденциальности</span>
+            <span className={styles.text}>{t('footer.privacy')}</span>
           </a>
           <a href="#" className={styles.link}>
             <ContactsIcon />
-            <span className={styles.text}>Контакты</span>
+            <span className={styles.text}>{t('footer.contacts')}</span>
           </a>
           <a href="#" className={styles.link}>
             <GithubIcon />
