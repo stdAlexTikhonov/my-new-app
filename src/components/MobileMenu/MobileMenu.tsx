da931556121898189f3dd611221eb322cfdbc3ae
Alexander <stdAlexTikhonov@hotmail.com>
2026-08-11 18:18:35 +0300

    fix(TASK-15): fix mobile menu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,15 +1,16 @@
-import { useTheme } from '@/context';
-import styles from './MobileMenu.module.scss';
-import clsx from 'clsx';
-import { GridIcon } from '../GridIcon';
-import { useTranslation } from 'react-i18next';
+import { useTheme } from "@/context";
+import styles from "./MobileMenu.module.scss";
+import clsx from "clsx";
+import { GridIcon } from "../GridIcon";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router";
 
 interface Props {
-    open: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const MobileMenu = ({ open, setOpen}: Props) => {
+export const MobileMenu = ({ open, setOpen }: Props) => {
   const { theme, toggleTheme } = useTheme();
   const { t, i18n } = useTranslation();
 
@@ -18,7 +19,7 @@ export const MobileMenu = ({ open, setOpen}: Props) => {
   };
 
   const toggleLanguage = () => {
-    const nextLang = i18n.language === 'en' ? 'ru' : 'en';
+    const nextLang = i18n.language === "en" ? "ru" : "en";
     changeLanguage(nextLang);
   };
 
@@ -26,45 +27,51 @@ export const MobileMenu = ({ open, setOpen}: Props) => {
 
   return (
     <div className={clsx(styles.menu, open && styles.opened)}>
-        <nav className={styles.menuNav}>
-            <a href="#" className={styles.menuLink} onClick={closeMenu}>
-            <span className={styles.menuIcon}><GridIcon hiddenCells={[0, 2, 7]} size={24} radius={15} /></span>
-            {t('mobilemenu.home')}
-            </a>
-            <a href="#" className={styles.menuLink} onClick={closeMenu}>
-            <span className={styles.menuIcon}><GridIcon hiddenCells={[1,2]} size={24} radius={15} /></span>
-            {t('mobilemenu.projects')}
-            </a>
-            <a href="#" className={styles.menuLink} onClick={closeMenu}>
-            <span className={styles.menuIcon}><GridIcon hiddenCells={[2,3,5,7]} size={24} radius={15} /></span>
-            {t('mobilemenu.blog')}
-            </a>
-        </nav>
-        <div className={styles.menuFooter}>
-            <button 
-                className={styles.menuThemeToggle}
-                onClick={() => {
-                    toggleTheme();
-                }}
-                aria-label={t('aria.toggleTheme')}
-                >
-                <span>{theme === 'light' ? '🌙' : '☀️'}</span>
-                <span className={styles.menuThemeLabel}>
-                    {theme === 'light' ? t('mobilemenu.dark') : t('mobilemenu.light')}
-                </span>
-            </button>
-            <button 
-                className={styles.menuThemeToggle}
-                onClick={() => {
-                    toggleLanguage();
-                }}
-                aria-label={t('aria.changeLanguage')}
-                >
-                <span className={styles.menuThemeLabel}>
-                    {t('mobilemenu.language')}
-                </span>
-            </button>
-        </div>
+      <nav className={styles.menuNav}>
+        <Link to="" className={styles.menuLink} onClick={closeMenu}>
+          <span className={styles.menuIcon}>
+            <GridIcon hiddenCells={[0, 2, 7]} size={24} radius={15} />
+          </span>
+          {t("mobilemenu.home")}
+        </Link>
+        <Link to="projects" className={styles.menuLink} onClick={closeMenu}>
+          <span className={styles.menuIcon}>
+            <GridIcon hiddenCells={[1, 2]} size={24} radius={15} />
+          </span>
+          {t("mobilemenu.projects")}
+        </Link>
+        <Link to="blog" className={styles.menuLink} onClick={closeMenu}>
+          <span className={styles.menuIcon}>
+            <GridIcon hiddenCells={[2, 3, 5, 7]} size={24} radius={15} />
+          </span>
+          {t("mobilemenu.blog")}
+        </Link>
+      </nav>
+      <div className={styles.menuFooter}>
+        <button
+          className={styles.menuThemeToggle}
+          onClick={() => {
+            toggleTheme();
+          }}
+          aria-label={t("aria.toggleTheme")}
+        >
+          <span>{theme === "light" ? "🌙" : "☀️"}</span>
+          <span className={styles.menuThemeLabel}>
+            {theme === "light" ? t("mobilemenu.dark") : t("mobilemenu.light")}
+          </span>
+        </button>
+        <button
+          className={styles.menuThemeToggle}
+          onClick={() => {
+            toggleLanguage();
+          }}
+          aria-label={t("aria.changeLanguage")}
+        >
+          <span className={styles.menuThemeLabel}>
+            {t("mobilemenu.language")}
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
